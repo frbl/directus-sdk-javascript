7975f13b6afc012f28b4aeab1186f9eec468979f
Frank Blaauw <frank.blaauw@gmail.com>
2018-08-21 07:52:44 +0200

    Implemented new webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,72 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+/* global __dirname, require, module*/
 
-module.exports = {
-  entry: './remote.js',
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+
+let libraryName = pkg.name;
+
+let plugins = [], outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin());
+  outputFile = libraryName + '.min.js';
+} else {
+  outputFile = libraryName + '.js';
+}
+
+/* Fix for Critical dependencies:
+9:12-34 the request of a dependency is an expression
+see https://berryware.wordpress.com/tag/iconv-loader/ */
+plugins.push(new webpack.NormalModuleReplacementPlugin(
+  /\/iconv-loader$/, 'node-noop'
+));
+
+const config = {
+  entry: __dirname + '/index.js',
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  },
+  devtool: 'source-map',
+  target: 'web',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'remote.js',
-    library: 'RemoteInstance'
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env']
-        }
+          'presets': [['es2015', {'modules': false}]]
+        },
+        exclude: /(node_modules|bower_components)/
       }
-    }]
+    ]
   },
-  plugins: [
-    new UglifyJsPlugin()
-  ]
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
+  plugins: plugins
 };
+
+module.exports = config;
